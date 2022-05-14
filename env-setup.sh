c3c60c27edc1b518445c5a2c1d88b4aa53188a74
Zhu Zhihao <abcde12345.z@gmail.com>
2022-05-14 14:23:39 +0800

    ammend previous commit

--- env-setup.sh
+++ env-setup.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+if [[ -z $(which unzip) ]]; then
+	echo "Cannot find unzip tool, try installing with package management tool first"
+	exit -1
+fi
+
 WORK_DIR=$(mktemp -d )
 # download package to tmp dir
-cp -r ./* $WORK_DIR/
+wget https://github.com/zhuzhihao/env-setup/releases/download/v0.0.1/release.zip -O $WORK_DIR/env-setup.zip
 
 pushd $WORK_DIR
+unzip env-setup.zip
 source scripts/bash-libraries/lib/liblog.bash
 SCRIPTS=$(find scripts/ -type f -name "*.sh"|sort -V)
 for scr in $SCRIPTS; do
@@ -15,6 +21,6 @@ for scr in $SCRIPTS; do
 done
 
 
-
+popd
 popd
 [[ $WORK_DIR =~ /tmp/.* ]] && rm -rfv $WORK_DIR
